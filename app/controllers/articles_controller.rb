2518bba65f6d0b0cc64cb4ceadd5db19bb7e6f59
Austen Bright <pureaustenbright@gmail.com>
2021-06-08 20:04:23 -0400

    new stuff

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :find_article, only: [:show, :edit, :update, :destroy]
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def index
@@ -13,14 +13,13 @@ class ArticlesController < ApplicationController
     end
 
     def edit    
-        @article = Article.find(params[:id])
     end
 
     def create
         # "require" and "permit" are needed to prevent a ForbiddenAttributesError 
         # this is a security feature in rails
         # "render" renders information to the screen
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
             #notice and alert
             flash[:notice] = "Article was created successfully"
@@ -31,13 +30,27 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfully."
             redirect_to @article
         else
             render 'edit'
         end
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private 
+
+    def find_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  root 'pages#home'
+  resources :articles 
 end
